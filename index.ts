1bee5bd5614d72a2585a07043115e8cb68f3c51b
Mingtian Yang <tswymt@gmail.com>
2022-11-13 15:30:13 +0800

    update package version

--- index.ts
+++ index.ts
@@ -2,11 +2,11 @@ import * as SSE50MOCK from './sse50Gateway'
 import * as RANDOMMOCK from './randomReadingGateway'
 
 if (process.env.DEBUG) {
-    SSE50MOCK.register().then(() => {
-        setInterval(() => {
-            SSE50MOCK.report()
-        }, 3000); // Time in milliseconds
-    })
+    // SSE50MOCK.register().then(() => {
+    //     setInterval(() => {
+    //         SSE50MOCK.report()
+    //     }, 3000); // Time in milliseconds
+    // })
     
     RANDOMMOCK.register().then(() => {
         setInterval(() => {
@@ -15,13 +15,13 @@ if (process.env.DEBUG) {
     })
 } else {
     console.log("Production Mode")
-    SSE50MOCK.register().then(() => {
-        console.log("Start Reporting SSE50MOCK")
+    // SSE50MOCK.register().then(() => {
+    //     console.log("Start Reporting SSE50MOCK")
         
-        setInterval(() => {
-            SSE50MOCK.report()
-        }, 60000); // Time in milliseconds， 1mins
-    })
+    //     setInterval(() => {
+    //         SSE50MOCK.report()
+    //     }, 60000); // Time in milliseconds， 1mins
+    // })
     
     RANDOMMOCK.register().then(() => {
         console.log("Start Reporting RANDOMMOCK")
